88b41dca053d480ce5e89e3d5ce0f8d8cf697c3d
Steph Reaves <stephanie.reaves@gmail.com>
2015-03-05 13:30:50 -0500

    tutorial 1 work

---
+++ JavaUdemy/tutorial1.java
@@ -0,0 +1,27 @@
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 92323;
+		
+		double myDouble = 8.23424;
+		float myFloat = 324.12f; 
+		
+		char myChar = 'y';
+		boolean myBoolean = true;
+		
+		byte myByte = 127; 
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
