ee708b306e8c79a2826103fdeb194fdde73a033c
Steph Reaves <stephanie.reaves@gmail.com>
2015-03-06 11:04:59 -0500

    for loops

---
+++ JavaUdemy/tutorial3.java
@@ -0,0 +1,12 @@
+public class Application {
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++){
+			System.out.printf("The value of i is: %d\n", i);
+		}
+	}
+
+}
+
+
+// could have used println, but alternatively printf (print format). You have to tell it The
+// two arguments first the string and second is the value to fill in the format specifier (symbol %d).
